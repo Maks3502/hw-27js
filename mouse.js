6161f630743901de0c03c5c4ed139c86d2b0d106
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-10-21 11:17:35 +0300

    Add files via upload

--- mouse.js
+++ mouse.js
@@ -1,59 +1,87 @@
-const boxImg = document.querySelectorAll("li");
-const img = document.querySelectorAll("img");
-const box = document.querySelector(".gallery");
-console.log(boxImg)
-console.log(img )
-console.log(box)
+// const boxImg = document.querySelectorAll("li");
+// const img = document.querySelectorAll("img");
+// const box = document.querySelector(".gallery");
+// console.log(boxImg)
+// console.log(img )
+// console.log(box)
 
-// for (let i = 0; i < boxImg.lenght; i++) {
-    let currentImageIndex = 1;
-   
-    window.addEventListener("keyprees", (event) =>{
+// let currentImageIndex  ;
 
-        currentImageIndex.classList.toggle("full-image-container")
-        img.classList.toggle("full-image")
 
-        if(event.key === "ArrowLeft"){
-    currentImageIndex(-1);
-}else if(event.key === "ArrowRight"){
-    currentImageIndex(1);
-}
-    })
-
+// for (let i = 0; i < img.length; i++) {
+//     img[i].addEventListener("click", (e) =>{
+        
+//         img[i].classList.toggle("full-image");
+//         img[i].parentElement.classList.toggle("full-image-container");
+//         currentImageIndex = i;
+//     })
 // }
 
 
-// document.addEventListener("keyup", flipThrough);
+//     window.addEventListener("keydown", (event) =>{
 
-// let currentImageIndex = 1;
+        
 
-// const flipThrough = (event) => {
-// if(event.key === "ArrowLeft"){
-//     currentImageIndex(-1);
+//         if(event.key === "ArrowLeft"){
+//            
+//             img[currentImageIndex].classList.toggle("full-image");
+//             img[currentImageIndex].parentElement.classList.toggle("full-image-container");
+//     currentImageIndex -= 1;
+//     if(currentImageIndex < 0){
+//         currentImageIndex = 0;
+//     }
+//     img[currentImageIndex].classList.toggle("full-image");
+//     img[currentImageIndex].parentElement.classList.toggle("full-image-container");
+    
 // }else if(event.key === "ArrowRight"){
-//     currentImageIndex(1);
+//     img[currentImageIndex].classList.toggle("full-image");
+//     img[currentImageIndex].parentElement.classList.toggle("full-image-container");
+// currentImageIndex += 1;
+// if(currentImageIndex >= img.length){
+// currentImageIndex = img.length -1;
 // }
+// img[currentImageIndex].classList.toggle("full-image");
+// img[currentImageIndex].parentElement.classList.toggle("full-image-container");
 // }
+//     })
+
+
+
+
+
 
 
 
 // завдання 2 
-// const container = document.querySelector(`[data-container]`);
-// const input = document.querySelector(`[data-input]`);
-// const buttonCreate = document.querySelectorAll(`[data-action="render"]`);
-// const buttonRemove = document.querySelectorAll(`[data-action="destroy"]`);
-// const divEmpty = document.querySelector(`[data-box-div]`);
+const container = document.querySelector(`[data-container]`);
+const input = document.querySelector(`[data-input]`);
+const buttonCreate = document.querySelector(`[data-action="render"]`);
+const buttonRemove = document.querySelector(`[data-action="destroy"]`);
+const divEmpty = document.querySelector(`[data-box-div]`);
+
+const arr = [];
+let size = 30;
+
+buttonCreate.addEventListener("click", () => {
+    createBoxes(input.value);
+});
 
 
-// buttonCreate.addEventListener("click", () => {
 
-// })
+function createBoxes (amount){
+    for(let i = 0; i < amount; i++){
+        const block = document.createElement("div");
+block.style.width = `${size}px`;
+block.style.height = `${size}px`;
+const r = Math.random() * 255;
+const g = Math.random() * 255;
+const b = Math.random() * 255;
+block.style.background = `rgb(${r}, ${g}, ${b},)`;
+   arr.push(block);
+   size += 10;
+    }
 
-
-// buttonCreate.removeEventListener("click", () => {
-
-// })
-
-// function createBoxes(amount) {
-
-// }
+    divEmpty.append(...arr);
+    
+    
+};
